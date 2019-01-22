ea6365f1907d4b6915975c5fa457c2c8d35c7e2f
High Sky <vasile123andronic@gmail.com>
2019-01-22 06:53:40 -0800

    stone game

--- gameP.py
+++ gameP.py
@@ -1,26 +1,22 @@
 
 pp=[]
 
-tot=10
+tot=10000
 
 M=tot+10
 
 for i in range(M):
     pp.append([0, 0])
 
-pp[1][0]=1/3
-pp[2][0]=1/3
-pp[3][0]=1/3
+pp[0][1]=1 #initialize
 
-# print(pp[1][1])
 for i in range(M):
     for j in range(3):
         k=j+1
-        if i>k:
+        if i>=k:
             pp[i][0]=pp[i][0]+pp[i-k][1]/3
-            # print("i={0}, k={1}, pp[i][0]={2}, pp[i-k][1]={3}".format(i, k, pp[i][0], pp[i-k][1]))
 
             pp[i][1] = pp[i][1] + pp[i-k][0] / 3
 
 
-print(pp[tot][0]*100)
+print("The probability Player1 will be winner : {} % ".format(pp[tot][0]*100))
